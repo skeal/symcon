4f2456bc7c6370891e309926d51ea9cbf026ab57
Blockmove <doc@poesl-online.de>
2015-07-05 17:22:54 +0200

    Update symcon_start.sh

--- symcon_start.sh
+++ symcon_start.sh
@@ -9,9 +9,9 @@
 #test if configuration in /etc/symcom exits.
 #if not -> copy template files
 if [ -e /etc/logo.ico ]; then
-    echo "Config exists"
+    echo "Config /etc exists"
 else
-    echo "Config not exists "
+    echo "Config /etc not exists "
     echo "Copy template files "
     cp -R /etc/symcon.org/* /etc/symcon
 fi
@@ -19,9 +19,9 @@ fi
 #test if configuration in /usr/share/symcom exits.
 #if not -> copy template files
 if [ -e /usr/share/symcon/settings.json ]; then
-    echo "Config exists"
+    echo "Config /usr/share exists"
 else
-    echo "Config not exists "
+    echo "Config /usr/share not exists "
     echo "Copy template files "
     cp -R /usr/share/symcon.org/* /usr/share/symcon
 fi
