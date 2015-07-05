ba6a3c04972528eea4b81b6a43f647b0d1429831
Dieter Poesl <doc@poesl-online.de>
2015-07-05 21:28:13 +0200

    changed copy conf-files

--- symcon_start.sh
+++ symcon_start.sh
@@ -6,6 +6,9 @@
 # 2015-07-05: Added test if configuration in "/usr/share/symcon" exit
 # ---------------
 
+# Versteckte Files (dot-files) bearbeiten (cp,mv,rm)
+shopt -s dotglob
+
 #test if configuration in /etc/symcom exits.
 #if not -> copy template files
 if [ -e /etc/logo.ico ]; then
@@ -24,6 +27,16 @@ else
     echo "Config /usr/share not exists "
     echo "Copy template files "
     cp -R /usr/share/symcon.org/* /usr/share/symcon
+fi
+
+#test if configuration in /root exits.
+#if not -> copy template files
+if [ -e /root/.profile ]; then
+    echo "Config /root exists"
+else
+    echo "Config /root not exists "
+    echo "Copy template files "
+    cp -R /root.org/* /root
 fi
 
 echo "Start IP-Symcon"
