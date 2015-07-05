855e4bb96ead5c76c2f76155969b93e4e24b3f6e
Blockmove <doc@poesl-online.de>
2015-07-05 12:07:53 +0200

    Fixed "etc" to "/etc"

--- symcon_start.sh
+++ symcon_start.sh
@@ -6,12 +6,12 @@
 
 #test if configuration in /etc/symcom exits.
 #if not -> copy template files
-if [ -e etc/logo.ico ]; then
+if [ -e /etc/logo.ico ]; then
     echo "Config exists"
 else
     echo "Config not exists "
     echo "Copy template files "
-    cp -R etc/symcon.org/* etc/symcon
+    cp -R /etc/symcon.org/* /etc/symcon
 fi
 
 echo "Start IP-Symcon"
