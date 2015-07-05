6d5cd04335790c9fe5277b50bc7263d5529b6e5f
Blockmove <doc@poesl-online.de>
2015-07-05 14:18:36 +0200

    Added test if configuration in "/usr/share/symcon"

--- symcon_start.sh
+++ symcon_start.sh
@@ -2,6 +2,8 @@
 
 # ---------------
 # Start IP-Symcon
+#
+# 2015-07-05: Added test if configuration in "/usr/share/symcon" exit
 # ---------------
 
 #test if configuration in /etc/symcom exits.
@@ -12,6 +14,16 @@ else
     echo "Config not exists "
     echo "Copy template files "
     cp -R /etc/symcon.org/* /etc/symcon
+fi
+
+#test if configuration in /usr/share/symcom exits.
+#if not -> copy template files
+if [ -e /usr/share/symcon/settings.json ]; then
+    echo "Config exists"
+else
+    echo "Config not exists "
+    echo "Copy template files "
+    cp -R /usr/share/symcon.org/* /usr/share/symcon
 fi
 
 echo "Start IP-Symcon"
