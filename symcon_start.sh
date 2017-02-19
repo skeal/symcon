007ea024882344412f8fbb673cba6ba4c518588d
root <root@gamma.home>
2017-02-19 18:55:52 +0100

    Update to IP-Symcon 4.1

--- symcon_start.sh
+++ symcon_start.sh
@@ -9,24 +9,24 @@
 # Versteckte Files (dot-files) bearbeiten (cp,mv,rm)
 shopt -s dotglob
 
-#test if configuration in /etc/symcom exits.
-#if not -> copy template files
-if [ -e /etc/logo.ico ]; then
-    echo "Config /etc exists"
-else
-    echo "Config /etc not exists "
-    echo "Copy template files "
-    cp -R /etc/symcon.org/* /etc/symcon
-fi
-
 #test if configuration in /usr/share/symcom exits.
 #if not -> copy template files
-if [ -e /usr/share/symcon/settings.json ]; then
+if [ -e /usr/share/symcon/logo.ico ]; then
     echo "Config /usr/share exists"
 else
     echo "Config /usr/share not exists "
     echo "Copy template files "
     cp -R /usr/share/symcon.org/* /usr/share/symcon
+fi
+
+#test if configuration in /var/lib/symcom exits.
+#if not -> copy template files
+if [ -e /var/lib/symcon/php.ini ]; then
+    echo "Config /var/lib exists"
+else
+    echo "Config /var/lib not exists "
+    echo "Copy template files "
+    cp -R /var/lib/symcon.org/* /var/lib/symcon
 fi
 
 #test if configuration in /root exits.
@@ -38,6 +38,10 @@ else
     echo "Copy template files "
     cp -R /root.org/* /root
 fi
+
+echo "Set timezone"
+ln -fs /usr/share/zoneinfo/Europe/Berlin /etc/localtime &&
+dpkg-reconfigure -f noninteractive tzdata
 
 echo "Start IP-Symcon"
 /usr/bin/symcon
